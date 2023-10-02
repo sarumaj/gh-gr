c70f516c7a2e57c4186d70fdabc71280d1c8a0fc
Dawid Ciepiela <zk791167@gmail.com>
2023-10-02 22:28:35 +0200

    fetch version as input parameter

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,21 +16,18 @@ supported_platforms=(
     windows-arm64
 )
 
-if [[ $GITHUB_REF = refs/tags/* ]]; then
-    VERSION="${GITHUB_REF#refs/tags/}"
-else
-    VERSION="$(git describe --tags --abbrev=0)"
-fi
-
+VERSION="$1"
 BUILD_DATE="$(date -u "+%Y-%m-%d %H:%M:%S UTC")"
 
 for p in "${supported_platforms[@]}"; do
     goos="${p%-*}"
     goarch="${p#*-}"
+
     ext=""
     if [ "$goos" = "windows" ]; then
         ext=".exe"
     fi
+
     GOOS="$goos" GOARCH="$goarch" CGO_ENABLED="${CGO_ENABLED:-0}" go build \
         -trimpath \
         -ldflags="-s -w -X 'main.Version=${VERSION}' -X 'main.BuildDate=${BUILD_DATE}' -extldflags=-static" \
