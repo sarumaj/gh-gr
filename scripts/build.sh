14de526a984d3714d6a5451859ddf295c331fb49
Dawid Ciepiela <zk791167@gmail.com>
2023-10-02 23:11:54 +0200

    added new steps

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,7 +19,6 @@ supported_platforms=(
 VERSION="$1"
 BUILD_DATE="$(date -u "+%Y-%m-%d %H:%M:%S UTC")"
 
-go install golang.org/x/tools/gopls@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 gofmt -s -d ./
@@ -39,4 +38,6 @@ for p in "${supported_platforms[@]}"; do
         -ldflags="-s -w -X 'main.Version=${VERSION}' -X 'main.BuildDate=${BUILD_DATE}' -extldflags=-static" \
         -tags="osusergo netgo static_build" \
         -o "dist/gh-gr_${VERSION}_${p}${ext}"
+
+    upx -best "dist/gh-gr_${VERSION}_${p}${ext}" -o "dist/gh-gr_${VERSION}_${p}${ext}"
 done
